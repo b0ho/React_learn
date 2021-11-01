c80f556c37873d71ef2f891723ce2f88948adff6
b0ho <ybg4828@gmail.com>
2021-11-01 16:08:49 +0900

    useState를 통해 props로 전달하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '1번', checked: true },
+    { id: 2, text: '2번', checked: true },
+    { id: 3, text: '3번', checked: false },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert></TodoInsert>
-      <TodoList></TodoList>
+      <TodoList todos={todos}></TodoList>
     </TodoTemplate>
   );
 };
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,14 +1,25 @@
+import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
-    <div className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert">
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
-        <MdAdd></MdAdd>
+        <MdAdd />
       </button>
-    </div>
+    </form>
   );
 };
 
