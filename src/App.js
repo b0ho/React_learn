4014de178e551fa42d9b134cd9c482477b5ebe10
b0ho <ybg4828@gmail.com>
2021-11-02 15:46:44 +0900

    produce를 함수 형태로 받기

--- src/App.js
+++ src/App.js
@@ -9,17 +9,14 @@ const App = () => {
     uselessValue: null,
   });
 
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm(
-        produce(form, (draft) => {
-          draft[name] = value;
-        }),
-      );
-    },
-    [form],
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      }),
+    );
+  }, []);
 
   const onSubmit = useCallback(
     (e) => {
@@ -30,7 +27,7 @@ const App = () => {
         username: form.username,
       };
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         }),
       );
@@ -40,22 +37,19 @@ const App = () => {
       });
       nextId.current += 1;
     },
-    [data, form.name, form.username],
+    [form.name, form.username],
   );
 
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1,
-          );
-        }),
-      );
-    },
-    [data],
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1,
+        );
+      }),
+    );
+  }, []);
 
   return (
     <div>
