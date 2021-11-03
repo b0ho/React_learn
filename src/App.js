35e25e6538ea39b814662cbf8b48612972ed291b
b0ho <ybg4828@gmail.com>
2021-11-03 23:58:55 +0900

    서브라우트 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return (
@@ -14,15 +14,12 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/my">my 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/you">you 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
