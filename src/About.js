ba6b4a7e9ea05010e5a2291950a70e0c2614909a
b0ho <ybg4828@gmail.com>
2021-11-03 23:51:28 +0900

    URL 쿼리로 요청
    qs 설치

--- src/About.js
+++ src/About.js
@@ -1,8 +1,17 @@
-const About = () => {
+import qs from 'qs';
+
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+
+  const showDetail = query.detail === 'true';
+
   return (
     <div>
       <h1>About</h1>
       <p>about 입니다.</p>
+      {showDetail && <p>detail 은 true</p>}
     </div>
   );
 };
