845c2104027e2983a76494473aa4cda79d7243bb
b0ho <ybg4828@gmail.com>
2021-11-04 23:28:40 +0900

    acync 적용

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((Response) => {
-        setData(Response.data);
-      });
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
