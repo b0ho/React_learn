ddfa29daf64bfa66c4890cd9d4a98abe02863d8d
b0ho <ybg4828@gmail.com>
2021-11-04 23:25:55 +0900

    axios로 데이터 받아오기

--- src/App.js
+++ src/App.js
@@ -1,76 +1,28 @@
-import { Route, Link, Switch } from 'react-router-dom';
-import About from './About';
-import Home from './Home';
-import Profiles from './Profiles';
-import History from './History';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  function increase(number) {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const result = number + 10;
-
-        if (result > 50) {
-          const e = new Error('NumberTooBig');
-          return reject(e);
-        }
-        resolve(result);
-      }, 1000);
-    });
-    return promise;
-  }
-
-  async function runTasks() {
-    try {
-      let result = await increase(0);
-      console.log(result);
-      result = await increase(result);
-      console.log(result);
-      result = await increase(result);
-      console.log(result);
-      result = await increase(result);
-      console.log(result);
-      result = await increase(result);
-      console.log(result);
-      result = await increase(result);
-      console.log(result);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  runTasks();
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((Response) => {
+        setData(Response.data);
+      });
+  };
 
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필</Link>
-        </li>
-        <li>
-          <Link to="/history">History</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path={['/about', '/info']} component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={History} />
-        <Route
-          render={({ location }) => (
-            <div>
-              <h2>존재하지 않는 페이지</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
         />
-      </Switch>
+      )}
     </div>
   );
 };
