5805cc49c5735f116d39a8748e2cd325967196dc
b0ho <ybg4828@gmail.com>
2021-11-18 22:57:58 +0900

    expree 적용

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,5 +1,23 @@
 import ReactDOMServer from "react-dom/server";
+import express from "express";
+import { StaticRouter } from "react-router-dom";
+import App from "./App";
 
-const html = ReactDOMServer.renderToString(<div>Hello</div>);
+const app = express();
 
-console.log(html);
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(root);
+};
+
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log("run on 5000");
+});
