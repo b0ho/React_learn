0a7b2249f2907cc292648680700f56438e86e650
b0ho <ybg4828@gmail.com>
2021-12-18 15:51:36 +0900

    태그 입력

--- blog/blog-frontend/src/components/write/TagBox.js
+++ blog/blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -14,7 +14,7 @@ const TagBoxBlock = styled.div`
   }
 `;
 
-const TagForm = styled.from`
+const TagForm = styled.form`
   border-radius: 4px;
   overflow: hidden;
   display: flex;
@@ -36,7 +36,7 @@ const TagForm = styled.from`
     cursor: pointer;
     padding-right: 1rem;
     padding-left: 1rem;
-    borer: none;
+    border: none;
     background: ${palette.gray[8]};
     color: white;
     font-weight: bold;
@@ -60,25 +60,59 @@ const TagListBlock = styled.div`
   margin-top: 0.5rem;
 `;
 
-const TagItem = React.memo(({ tag }) => <Tag>#{tag}</Tag>);
+const TagItem = React.memo(({ tag, onRemove }) => (
+  <Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
 
-const TagList = React.memo(({ tags }) => (
+const TagList = React.memo(({ tags, onRemove }) => (
   <TagListBlock>
     {tags.map((tag) => (
-      <TagItem key={tag} tag={tag} />
+      <TagItem key={tag} tag={tag} onRemove={onRemove} />
     ))}
   </TagListBlock>
 ));
 
 const TagBox = () => {
+  const [input, setInput] = useState('');
+  const [localTags, setLocalTags] = useState([]);
+
+  const insertTag = useCallback(
+    (tag) => {
+      if (!tag) return;
+      if (localTags.includes(tag)) return;
+      setLocalTags([...localTags, tag]);
+    },
+    [localTags],
+  );
+
+  const onRemove = useCallback(
+    (tag) => {
+      setLocalTags(localTags.filter((t) => t !== tag));
+    },
+    [localTags],
+  );
+
+  const onChange = useCallback((e) => {
+    setInput(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      insertTag(input.trim());
+      setInput('');
+    },
+    [input, insertTag],
+  );
+
   return (
     <TagBoxBlock>
       <h4>태그</h4>
-      <TagForm>
-        <input placeholder="태그" />
+      <TagForm onSubmit={onSubmit}>
+        <input placeholder="태그" value={input} onChange={onChange} />
         <button type="submit">추가</button>
       </TagForm>
-      <TagList tags={['태그1', '태그2']} />
+      <TagList tags={localTags} onRemove={onRemove} />
     </TagBoxBlock>
   );
 };
